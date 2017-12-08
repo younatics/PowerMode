ef8a1032832d59e0b825d5c494f9ca50492fe7d1
Seunyoun Yi <younatics@gmail.com>
2017-12-08 17:25:46 +0900

    Initial commit

---
+++ PowerMode/PowerMode.h
@@ -0,0 +1,19 @@
+//
+//  PowerMode.h
+//  PowerMode
+//
+//  Created by USER on 2017. 12. 8..
+//  Copyright © 2017년 Seungyounyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PowerMode.
+FOUNDATION_EXPORT double PowerModeVersionNumber;
+
+//! Project version string for PowerMode.
+FOUNDATION_EXPORT const unsigned char PowerModeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PowerMode/PublicHeader.h>
+
+
