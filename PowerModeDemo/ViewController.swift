83cc8afcbc019a67bde819189ec5a5ad31fb39e1
Seungyoun Yi <younatics@gmail.com>
2017-12-09 23:15:10 +0900

    add demo file

--- PowerModeDemo/ViewController.swift
+++ PowerModeDemo/ViewController.swift
@@ -10,31 +10,16 @@ import UIKit
 import PowerMode
 
 class ViewController: UIViewController {
-    @IBOutlet var demoTextView: PowerModeTextView!
-    
+    @IBOutlet var powerModeTextView: PowerModeTextView!
+    @IBOutlet var powerModeTextField: PowerModeTextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-//        demoTextView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
-//    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
-//        let beginning = textView.beginningOfDocument
-//        let start = textView.position(from: beginning, offset: range.location)
-//        let end = textView.position(from: start!, offset: range.length)
-//        let textRange = textView.textRange(from: start!, to: end!)
-//
-//        let rect = textView.firstRect(for: textRange!)
-//        SparkAction.shared.at(position: CGPoint(x: rect.origin.x, y: rect.origin.y), with: UIColor.red, in: textView)
-//
-//        return true
-//    }
-    
-    
     
 }
 
