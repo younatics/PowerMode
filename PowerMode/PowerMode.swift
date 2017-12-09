4b248b65cb4ad6517ecc5a8ccec69c60165a325f
Seungyoun Yi <younatics@gmail.com>
2017-12-09 22:47:20 +0900

    Code refacrtor

---
+++ PowerMode/PowerMode.swift
@@ -0,0 +1,27 @@
+//
+//  PowerMode.swift
+//  PowerMode
+//
+//  Created by YiSeungyoun on 2017. 12. 9..
+//  Copyright © 2017년 Seungyounyi. All rights reserved.
+//
+
+import UIKit
+
+class PowerMode: NSObject {
+    class func animate(in textInput: UITextInput, with range:NSRange) -> CGRect? {
+        let beginning = textInput.beginningOfDocument
+        let start = textInput.position(from: beginning, offset: range.location)
+        
+        guard let _start = start else { return nil }
+        let end = textInput.position(from: _start, offset: range.length)
+        
+        guard let _end = end else { return nil }
+        let textRange = textInput.textRange(from: _start, to: _end)
+        
+        guard let _textRange = textRange else { return nil }
+        let rect = textInput.firstRect(for: _textRange)
+        
+        return rect
+    }
+}
--- PowerMode/PowerModeTextField.swift
+++ PowerMode/PowerModeTextField.swift
@@ -1,31 +1,32 @@
 //
-//  PowerModeTextView.swift
+//  PowerModeTextField.swift
 //  PowerMode
 //
-//  Created by YiSeungyoun on 2017. 12. 8..
+//  Created by YiSeungyoun on 2017. 12. 9..
 //  Copyright © 2017년 Seungyounyi. All rights reserved.
 //
 
 import UIKit
 
-public class PowerModeTextView: UITextView, UITextViewDelegate {
-    override public init(frame: CGRect, textContainer: NSTextContainer?) {
-        super.init(frame: frame, textContainer: textContainer)
+public class PowerModeTextField: UITextField, UITextFieldDelegate {
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
         
         self.delegate = self
     }
     
-    required public init?(coder aDecoder: NSCoder) {
+    public required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         
         self.delegate = self
     }
     
-    public func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+    public func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         guard let rect = PowerMode.animate(in: textView, with: range), let superView = self.superview else { return true }
         let convertedRect = convert(rect, to: superView)
         
         SparkAction.shared.at(position: CGPoint(x: convertedRect.origin.x, y: convertedRect.origin.y), with: UIColor.red, in: superView)
+
         return true
     }
 }
