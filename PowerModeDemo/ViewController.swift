b254f182754bd61bafceb2e8fd822d11c5a8e936
Seungyoun Yi <younatics@gmail.com>
2017-12-10 11:50:53 +0900

    Pantone color

--- PowerModeDemo/ViewController.swift
+++ PowerModeDemo/ViewController.swift
@@ -17,11 +17,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         // Spark values
-        PowerMode.sparkColors = [UIColor.red, UIColor.orange, UIColor.yellow, UIColor.green, UIColor.blue, UIColor.purple, UIColor.brown]
+        PowerMode.sparkColors = [UIColor(hexString: "#9dc6d8"), UIColor(hexString: "#00b3ca"), UIColor(hexString: "#7dd0b6")]
+//        PowerMode.sparkColors = [UIColor.red, UIColor.orange, UIColor.yellow, UIColor.green, UIColor.blue, UIColor.purple, UIColor.brown]
         PowerMode.isSparkActionEnabled = true
 
         // Shake values
-        PowerMode.isShakeActionEnabled = false
+        PowerMode.isShakeActionEnabled = true
         PowerMode.shakeTranslationX = 0
         PowerMode.shakeTranslationY = 2
     }
@@ -33,3 +34,28 @@ class ViewController: UIViewController {
 }
 
 
+
+extension UIColor {
+    convenience init(hexString:String) {
+        let hexString          = hexString.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        let scanner            = Scanner(string: hexString)
+        
+        if (hexString.hasPrefix("#")) {
+            scanner.scanLocation = 1
+        }
+        
+        var color:UInt32 = 0
+        scanner.scanHexInt32(&color)
+        
+        let mask = 0x000000FF
+        let r = Int(color >> 16) & mask
+        let g = Int(color >> 8) & mask
+        let b = Int(color) & mask
+        
+        let red   = CGFloat(r) / 255.0
+        let green = CGFloat(g) / 255.0
+        let blue  = CGFloat(b) / 255.0
+        
+        self.init(red:red, green:green, blue:blue, alpha:1)
+    }
+}
